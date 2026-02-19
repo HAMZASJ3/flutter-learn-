8fa829eb68e9cbbd18ff28eeb6fd511437ce0faf
HamzaSJ3 <hamzehalhaj5@gmail.com>
2026-02-20 00:24:04 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,10 @@
-import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
 
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized();
 
-  runApp(EasyLocalization(
-      supportedLocales: [Locale('en'), Locale('ar')],
-      path: 'assets/translations', // <-- change the path of the translation files
-      fallbackLocale: Locale('en'),
-      child:  MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -21,15 +14,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo H',
+
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
       ),
-      home:
 
     );
   }
