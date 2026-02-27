5ce326194bbd39e00117c0ed4d70198be57e95cf
HamzaSJ3 <hamzehalhaj5@gmail.com>
2026-02-27 04:21:52 +0300

    test

---
+++ lib/core/utils/database_helper.dart
@@ -0,0 +1,35 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+
+class DatabaseHelper {
+  /// انشاء اوبجكت من الكلاس لمرة واحدة
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+
+  factory DatabaseHelper() => _instance;
+
+  static Database? _database;
+
+  DatabaseHelper._internal();
+
+  Future<Database> get database async {
+    // check if database exists
+    if (_database != null)
+      return _database!;
+
+    _database = await _initDatabase();
+    return _database!;
+  }
+  Future<Database> _initDatabase() async {
+    final String path = join(await getDatabasesPath(), 'items.db');
+    return await openDatabase(path, version: 1, onCreate: _createDatabase);
+  }
+  Future<void> _createDatabase(Database db, int version) async {
+await db.execute('''
+CREATE TABLE items(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  barcode TEXT NOT NULL,
+  price REAL NOT NULL
+''');
+  }
